18f2067545851be08bf14efb5e52110d2485bfd8
Dulche <115841388+kevin-dulche@users.noreply.github.com>
2023-08-10 10:42:59 -0600

    POO - 8 de agosto
    Agregué los archivos del día 9 de agosto

---
+++ 4.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+float calculadora(float a, float b, int opcion){
+    switch (opcion)
+    {
+    case 1:
+        return a + b;
+        break;
+    case 2:
+        return a - b;
+        break;
+    case 3:
+        return a * b;
+        break;
+    case 4:
+        if (b == 0) {
+            printf("No se puede dividir por cero.\n");
+            return 0; // O alguna acción apropiada
+        }
+        return a / b;
+        break;
+    default:
+        break;
+    }
+}
+
+int main(){
+    int opc;
+    int v1, v2;
+    float resultado;
+    while (1){
+        printf("\n\tMENU\n");
+        printf("1.- Suma\n");
+        printf("2.- Resta\n");
+        printf("3.- Multiplicacion\n");
+        printf("4.- Division\n");
+        printf("5.- Salir\n");
+        printf("Ingresa la opcion que desea: ");
+        scanf("%d", &opc);
+        if (opc != 1 && opc != 2 && opc != 3 && opc != 4 && opc != 5 )
+        {
+            printf("Respuesta erronea\n");
+            continue;
+        }
+        if (opc == 5)
+        {
+            exit(0);
+        }
+        
+        printf("Dame el primer numero: ");
+        scanf("%d", &v1);
+        printf("Dame el segundo numero: ");
+        scanf("%d", &v2);
+
+        resultado = calculadora(v1, v2, opc);
+        printf("El resultado es: %f\n", resultado);
+    }
+    return 0;
+}
