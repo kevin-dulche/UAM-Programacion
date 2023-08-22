b9be20f8717eabfc36acf2a9736656515ea70cc0
Kevin Dulche Laptop Windows <kevdulche@gmail.com>
2023-08-21 21:38:01 -0600

    Correccion

--- PooCC01CNetz/Semana3/Java/Ejercicios1/src/Escuela.java
+++
@@ -1,9 +0,0 @@
-public class Escuela {
-    public static void main(String[] args) {
-        Alumno alu = new Alumno();
-        alu.setNombre("Juan");
-        alu.setMatricula(12345);
-        System.out.println("Nombre: " + alu.getNombre());
-        System.out.println("Matricula: " + alu.getMatricula());
-    }
-}
